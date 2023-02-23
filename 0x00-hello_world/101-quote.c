52a2a4ea922e89554da331f811b8a060277bcbe0
tayyib 4 tech <gbondotahib72@gmail.com>
2023-02-23 09:32:33 -0800

    all completed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 
 int main(void)
 {
-	write("\nand that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
 	return (0);
 }
