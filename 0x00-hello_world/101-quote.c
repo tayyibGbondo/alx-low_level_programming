f35ff86c3346db5729ad6737ccd88f915e80763f
tayyib 4 tech <gbondotahib72@gmail.com>
2023-02-23 09:21:20 -0800

    advanced tasks complete

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - outputting without printf and puts
+ * Return:1
+ */
+
+int main(void)
+{
+	write("\nand that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
