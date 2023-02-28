8f969b56ed31e735d007d70f8e9376cbbf72af19
Tayyib Gbondo <77306354+tayyibGbondo@users.noreply.github.com>
2023-02-28 22:56:33 +0000

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 numbers
+ * @a: paramater
+ * @b: parameter
+ * Return: 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers between 0 - 9 excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
